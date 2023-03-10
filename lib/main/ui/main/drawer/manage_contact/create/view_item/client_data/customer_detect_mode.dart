66e401b8c1e9a0d6110b4e447d5494bfdda658d8
Lộc phụ hồ <builoc12a3@gmail.com>
2023-03-10 18:18:23 +0700

    update detect identity

---
+++ lib/main/ui/main/drawer/manage_contact/create/view_item/client_data/customer_detect_mode.dart
@@ -0,0 +1,16 @@
+class CustomerDetectModel {
+  String? type;
+  int? idNumber;
+  String? lastName;
+  String? name;
+  String? fullName;
+  String? nationality;
+  String? sex;
+  String? dateOfBirth;
+  String? expiredDate;
+  String? address;
+  String? province;
+  String? district;
+  String? precinct;
+  String? image;
+}
