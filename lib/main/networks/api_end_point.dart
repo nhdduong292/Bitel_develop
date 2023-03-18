0e80b5635ee203201ba77077845fb2c019c4a05f
Duong Nguyen <huuduong996@gmail.com>
2023-03-18 08:36:20 +0700

    update

--- lib/main/networks/api_end_point.dart
+++ lib/main/networks/api_end_point.dart
@@ -1,52 +1,52 @@
 class ApiEndPoints {
-  static final String DOMAIN = "http://10.121.14.196:9092";
-  static final String DOMAIN_TEST = "http://10.121.14.196:9092";
-  static final bool isDev = true;
-  static final String API_LIST_REQUEST =
+  static const String DOMAIN = "http://10.121.14.196:9092";
+  static const String DOMAIN_TEST = "http://10.121.14.196:9092";
+  static const bool isDev = true;
+  static const String API_LIST_REQUEST =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/requests";
-  static final String API_CREATE_REQUEST =
+  static const String API_CREATE_REQUEST =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/requests";
-  static final String API_CHANGE_STATUS_REQUEST =
+  static const String API_CHANGE_STATUS_REQUEST =
       "/change-status"; //todo dùng cho cả survey offline
-  static final String API_REQUEST_DETAIL =
+  static const String API_REQUEST_DETAIL =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/requests";
-  static final String API_TRANSFER_REQUEST = "/transfer";
-  static final String API_REASONS =
+  static const String API_TRANSFER_REQUEST = "/transfer";
+  static const String API_REASONS =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/reasons";
-  static final String API_SEARCH_CONTACT =
+  static const String API_SEARCH_CONTACT =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/contacts";
-  static final String API_PROVINCES =
+  static const String API_PROVINCES =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/areas/provinces";
-  static final String API_PRECINCTS =
+  static const String API_PRECINCTS =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/areas/precincts";
-  static final String API_DISTRICTS =
+  static const String API_DISTRICTS =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/areas/districts";
-  static final String API_SURVEY = "${isDev ? DOMAIN_TEST : DOMAIN}/v1/surveys";
-  static final String API_SURVEY_ONLINE = "/lock";
-  static final String API_HOME_SALE =
+  static const String API_SURVEY = "${isDev ? DOMAIN_TEST : DOMAIN}/v1/surveys";
+  static const String API_SURVEY_ONLINE = "/lock";
+  static const String API_HOME_SALE =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/home-page/statistic";
-  static final String API_LIST_PRODUCT =
+  static const String API_LIST_PRODUCT =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/products";
-  static final String API_CUSTOMER =
+  static const String API_CUSTOMER =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/customers/registered";
-  static final String API_CREATE_CONTRACT =
+  static const String API_CREATE_CONTRACT =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/contracts";
-  static final String API_CONTRACT_PREVIEW =
+  static const String API_CONTRACT_PREVIEW =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/contracts/id/preview";
-  static final String API_LOGIN = "http://10.121.14.196:9093/login";
+  static const String API_LOGIN = "http://10.121.14.196:9093/login";
 
-  static final String API_PLAN_REASON =
+  static const String API_PLAN_REASON =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/plan-reasons";
-  static final String API_WALLET =
+  static const String API_WALLET =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/wallets/balance";
-  static final String API_BEST_FINGER =
+  static const String API_BEST_FINGER =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/customers/id/best-finger";
-  static final String API_SIGN_CONTRACT =
+  static const String API_SIGN_CONTRACT =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/contracts/id/sign";
 
-  static final String API_DETECT_ID = "https://vision.googleapis.com/v1/images:annotate";
-  static final String API_CREATE_CUSTOMER =
+  static const String API_DETECT_ID = "https://vision.googleapis.com/v1/images:annotate";
+  static const String API_CREATE_CUSTOMER =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/customers";
-  static final String API_REGISTER_FINGER =
+  static const String API_REGISTER_FINGER =
       "${isDev ? DOMAIN_TEST : DOMAIN}/v1/customers/id/register-finger";
 }
