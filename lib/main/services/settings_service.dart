62c1ebe156b342e6cbbc369d899c6401486d0fbb
Lộc phụ hồ <builoc12a3@gmail.com>
2023-07-26 09:09:31 +0700

    update version

--- lib/main/services/settings_service.dart
+++ lib/main/services/settings_service.dart
@@ -44,7 +44,7 @@ class SettingService extends GetxService {
   late SharedPreferences prefs;
 
   final Rx<String> token = "".obs;
-  Rx<String> version = "1.0.23".obs;
+  Rx<String> version = "1.0.24".obs;
 
   Future<SettingService> init() async {
     prefs = await SharedPreferences.getInstance();
